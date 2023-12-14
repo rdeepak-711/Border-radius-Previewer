cdf47fff251f4b9bede91d6e9cef8828b7b4481c
DeepakR-UiPath <124669154+DeepakR-UiPath@users.noreply.github.com>
2023-12-14 16:02:39 +0530

    Basic Website is set up with the design

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='container-fluid vh-100 d-flex flex-column justify-content-center align-items-center bg-light'>
+      <div>
+        <p className="h2 text-black">Border Radius Previewer</p>
+      </div>
+      <div class="square square-lg bg-secondary h-50 w-25">
+        
+      </div>
+      <div className='d-flex flex-row input-group m-3 justify-content-center align-items-center'>
+        <div className='p-2 mx-2'>
+          <span class="input-group-text" id="borderTopRight">Top Right</span>
+          <input type="number" id="borderTopRight" name="borderTopRight"/>
+        </div>
+        <div className='p-2 mx-2'>
+          <span class="input-group-text" id="borderTopLeft">Top Left</span>
+          <input type="number" id="borderTopLeft" name="borderTopLeft"/>
+        </div>
+        <div className='p-2 mx-2'>
+          <span class="input-group-text" id="borderBottomRight">Bottom Right</span>
+          <input type="number" id="borderBottomRight" name="borderBottomRight"/>
+        </div>
+        <div className='p-2 mx-2'>
+          <span class="input-group-text" id="borderBottomLeft">Bottom Left</span>
+          <input type="number" id="borderBottomLeft" name="borderBottomLeft"/>
+        </div>
+      </div>
+    </div>
   );
 }
 
